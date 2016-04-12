bfdefc815c142d7f0dc1d597310e216933bedce9
Gabriel Andretta <gabriel.andretta@gmail.com>
2016-04-12 19:31:41 -0300

    Copy components to the webpack build

--- component.js
+++ component.js
@@ -1,3 +1,67 @@
 import React from 'react';
 
-export default () => <div>Component</div>;
+const Loading = () => (
+  <div className="spinner spinner-center spinner-lg is-auth0">
+    <div className="circle"></div>
+  </div>
+);
+
+const SocialProvider = ({str}) => (
+  <span className="auth0-lock-social-button" data-provider={str}>
+    <div className="auth0-lock-social-button-icon"></div>
+  </span>
+);
+
+const UserItem = ({user}) => {
+  const maybeSocialProvider = user.socialProviders.length > 0
+    ? <SocialProvider str={user.socialProviders[0]} />
+    : null;
+
+  return (
+    <div className="user">
+      <div className="tile">
+        <img src={user.picture} />
+        {maybeSocialProvider}
+      </div>
+
+      <div className="information">
+        <a href="#" className="back"><i className="material-icons">keyboard_backspace</i></a>
+
+        <div className="content">
+          <div className="picture">
+            <img src={user.picture} />
+            {maybeSocialProvider}
+          </div>
+
+          <h2 className="name truncate">{user.name}</h2>
+          <p className="secondary">Frontend Developer</p>
+          <p className="additional">I like unicorns and bold fonts.</p>
+          <a className="btn btn-lg btn-success" href="#"><span className="btn-icon icon-budicon-493"></span>Look up</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const UserGrid = ({users}) => (
+ <span>{ users.map((x, i) => <UserItem key={i} user={x} />)}</span>
+);
+
+const Stats = ({stats}) => (
+  <span>22 users logged in today.</span>
+);
+
+
+export default ({users, stats}) => {
+  return (
+    <div className="auth0-users-widget">
+      <h1>Latest Logins</h1>
+      <div className="grid clearfix">
+        <UserGrid users={users.recent} />
+      </div>
+      <p className="footer">
+        <Stats stats={stats} />
+      </p>
+    </div>
+  );
+}
--- index.js
+++ index.js
@@ -2,7 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './component';
 
-const state = {};
+const state = {
+    stats: {
+      error: false,
+      logins: 43,
+    },
+    users: {
+      error: false,
+      full: true,
+      recent: [
+        {
+          name: "Gabriel Andretta",
+          nickname: "Gabriel Andretta",
+          picture: "https:\/\/pbs.twimg.com\/profile_images\/378800000053651919\/27246a03737f41dd06633bfd74ca9f3c_normal.png",
+          socialProviders: ["twitter"]
+        },
+        {
+          email: "gabriel.andretta@gmail.com",
+          name: "gabriel.andretta@gmail.com",
+          nickname: "gabriel.andretta",
+          picture: "https:\/\/s.gravatar.com\/avatar\/05a4170856a6157f81319595e42fdc5b?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fga.png",
+          socialProviders: ["github"]
+        },
+        {
+          email: "gabriel.andretta@gmail.com",
+          name: "gabriel.andretta@gmail.com",
+          nickname: "gabriel.andretta",
+          picture: "https:\/\/s.gravatar.com\/avatar\/05a4170856a6157f81319595e42fdc5b?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fga.png",
+          socialProviders: ["instagram"]
+        },
+        {
+          email: "gabriel.andretta+18@gmail.com",
+          name: "gabriel.andretta+18@gmail.com",
+          nickname: "gnandretta",
+          picture: "https:\/\/s.gravatar.com\/avatar\/c7d9610710f2ec82c65dca071de0e259?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fgn.png",
+          socialProviders: ["github"],
+          username: "gnandretta"
+        },
+        {
+          email: "gabriel.andretta+17@gmail.com",
+          name: "gabriel.andretta+17@gmail.com",
+          nickname: "gabriel.andretta+17",
+          picture: "https:\/\/s.gravatar.com\/avatar\/a2b30d4a1c5b82657f191ee24533fbd3?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fga.png",
+          socialProviders: []
+        },
+        {
+          email: "gabriel.andretta+16@gmail.com",
+          name: "gabriel.andretta+16@gmail.com",
+          nickname: "gabriel.andretta+16",
+          picture: "https:\/\/s.gravatar.com\/avatar\/8e5de6e11045344267dbcd9ec746467c?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fga.png",
+          socialProviders: [],
+        },
+        {
+          email: "gabriel.andretta+15@gmail.com",
+          name: "gabriel.andretta+15@gmail.com",
+          nickname: "gabriel.andretta+15",
+          picture: "https:\/\/s.gravatar.com\/avatar\/ba0aa404d89e097a7c135e74217b9659?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fga.png",
+          socialProviders: []
+        },
+        {
+          email: "gabriel.andretta+14@gmail.com",
+          name: "gabriel.andretta+14@gmail.com",
+          nickname: "gabriel.andretta+14",
+          picture: "https:\/\/s.gravatar.com\/avatar\/2ee5b87c91f911c5734ece66eaba8ec0?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fga.png",
+          socialProviders: []
+        },
+        {
+          email: "gabriel.andretta+13@gmail.com",
+          name: "gabriel.andretta+13@gmail.com",
+          nickname: "gnandretta13",
+          picture: "https:\/\/s.gravatar.com\/avatar\/374b0873408e20cde31a6759ccc4c67b?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fgn.png",
+          socialProviders: [],
+          username: "gnandretta13"
+        }
+      ]
+    }
+};
 
 ReactDOM.render(
   <Component {...state} />,
